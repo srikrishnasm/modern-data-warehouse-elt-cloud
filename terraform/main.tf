ab9639d26baa3fe29649b08eaab6a4d0b793274f
srikrishnasm <srikrishnasm19@gmail.com>
2026-04-02 22:42:21 +0530

    Phase 4 : DBT docs and lineage graph

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,6 +65,14 @@ resource "aws_security_group" "airflow_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "dbt docs"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
